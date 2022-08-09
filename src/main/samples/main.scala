9a2b6e9fcd93c818722eaab35e8c279f0aca234d
Daniela <83104187+Dani141@users.noreply.github.com>
2022-08-08 21:14:18 -0400

    commit

---
+++ src/main/samples/main.scala
@@ -0,0 +1,81 @@
+package main.samples
+
+import com.citi.ml.FP_Outlier
+import com.citi.transformations._
+import org.apache.spark.sql._
+import org.apache.spark.sql.functions.{concat_ws, lit, monotonically_increasing_id}
+
+object main {
+  def main(args: Array[String]): Unit = {
+    val spark=SparkSession.builder().master("local[*]").appName("TEST").getOrCreate()
+    spark.sparkContext.setLogLevel("ERROR")
+    import spark.implicits._
+
+    //Carega de datos original
+    var data= spark.read.option("header","true").option("inferSchema","true").csv("data/dataset/mammography_id.csv")
+      .drop("ID")
+    try {
+      //Carega de datos temporal
+      var dataTemporary = spark.read.option("header", "true").option("inferSchema", "true").csv("data/temporaryBasis")
+        .drop("ID", "features", "LFPOF_METRIC", "FPOF_METRIC", "WCPOF_METRIC")
+      for (d <- dataTemporary.columns)
+        if (d.contains("_bin"))
+          dataTemporary = dataTemporary.drop(d)
+      //Unir los dos datos
+      data = dataTemporary.union(data).withColumn("ID", monotonically_increasing_id())
+    }catch {
+      case e: Exception =>{}
+    }
+
+    var bin = data
+    for (d <- data.columns){
+      if (!d.equals("ID") && !d.equals("Class") && !d.contains("_bin")){
+        //Discretizacón de los intervalos en 6
+        val bin1= new EqualRangeBinner()
+          .setNumBuckets(6)
+          .setInputColName(d)
+          .setOutputColName(d +"_bin")
+          .fit(bin)
+          .transform(bin)
+        bin = bin1
+      }
+    }
+
+    //Discretizacón de los intervalos en 6
+    val algLFPOF= new FP_Outlier()
+      .setMinConfidence(0.6)
+      .setMinSupport(0.1)
+      .setColumns(bin.columns
+      .filter(x=>x.contains("_bin")))
+      .train(bin)
+
+    //Ejecución del algoritmo
+    var result= algLFPOF.transform(bin,spark)
+    //Definir si es anomalo
+    var c = isAbnormal(algLFPOF.lfpopAbnormal, algLFPOF.wcpofAbnormal,result.select("LFPOF_METRIC","WCPOF_METRIC").collect())
+
+    result.show(160,false)
+    //Escribiendo resultados
+    result.withColumn("features", stringify(result.col("features")))
+      .write
+      .mode(SaveMode.Overwrite)
+      .option("header","true")
+      .csv("data/temporaryBasis")
+  }
+  //Procesando arrays para guardarlos en csv
+  def stringify(c: Column) = functions.concat(lit("["), concat_ws(",", c), lit("]"))
+  def isAbnormal(lfpofMinValue: Double, wcpofMinValue: Double, rows: Array[Row]): List[String] ={
+    var is:List[String]=List()
+    for(r<-rows){
+      if(r.get(0)==lfpofMinValue)
+        is = is:+"true"
+      else
+        if(r.get(1)==wcpofMinValue)
+          is = is:+"true"
+        else
+          is = is:+"false"
+
+    }
+    is
+  }
+}
